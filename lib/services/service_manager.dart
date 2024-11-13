ea3068a779827f19ed5f8c7603b1f3558017878b
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-13 23:46:30 +0200

    Define a Global Instance in service_manager.dart

--- lib/services/service_manager.dart
+++ lib/services/service_manager.dart
@@ -4,16 +4,19 @@ import 'receipt_service.dart';
 import 'user_service.dart';
 
 class ServiceManager {
-  // Singleton pattern for global access
+  // Singleton pattern
   static final ServiceManager _instance = ServiceManager._internal();
 
   factory ServiceManager() => _instance;
 
   // Initialize each service instance
+  final BudgetService budgetService = BudgetService();
   final UserService userService = UserService();
   final CategoryService categoryService = CategoryService();
-  final BudgetService budgetService = BudgetService();
   final ReceiptService receiptService = ReceiptService();
 
   ServiceManager._internal();
 }
+
+// Define a global instance
+final services = ServiceManager();
