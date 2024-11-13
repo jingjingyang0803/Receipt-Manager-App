8d8c7ea447c3b3c2e4f0c6543f381e354f68f1df
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-13 23:23:17 +0200

    Improve LogoutPopup component

--- lib/components/logout_popup.dart
+++ lib/components/logout_popup.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:receipt_manager/constants/app_colors.dart';
+
+import 'custom_button.dart';
 
 class LogoutPopup extends StatelessWidget {
   final VoidCallback onConfirm;
@@ -9,71 +12,76 @@ class LogoutPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
+    return Container(
+      padding: EdgeInsets.symmetric(
+          horizontal: 20, vertical: 12), // Reduced vertical padding
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
       ),
-      title: Column(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Divider(
-            thickness: 2,
-            color: Color(0xFFEEE5FF), // You can use purple20 here if defined
+            thickness: 3,
+            color: purple40,
+            endIndent: 165,
+            indent: 165,
           ),
           SizedBox(height: 8),
           Text(
             'Logout?',
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 18,
               fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
           ),
+          SizedBox(height: 8),
+          Text(
+            'Are you sure do you wanna logout?',
+            style: TextStyle(
+              fontSize: 18,
+              color: light20,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomButton(
+                      text: "No",
+                      backgroundColor: purple20,
+                      textColor: purple100,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      } // Close the popup},
+                      ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomButton(
+                    text: "Yes",
+                    backgroundColor: purple100,
+                    textColor: light80,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
         ],
       ),
-      content: Text(
-        'Are you sure you want to logout?',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.grey,
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: onCancel,
-          style: TextButton.styleFrom(
-            backgroundColor:
-                Color(0xFFFDD5D7), // Use red20 for cancel button background
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: Text(
-            'No',
-            style: TextStyle(
-              color: Color(0xFFFD3C4A), // Use red100 for text color
-              fontSize: 16,
-            ),
-          ),
-        ),
-        TextButton(
-          onPressed: onConfirm,
-          style: TextButton.styleFrom(
-            backgroundColor: Color(
-                0xFF7F3DFF), // Use purple100 for confirm button background
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: Text(
-            'Yes',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
