0e65b0c3613d09c49e91f37851da964bdc5b25ab
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-15 17:19:03 +0200

    Improve style of DateRangeContainer

--- lib/components/date_range_container.dart
+++ lib/components/date_range_container.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../constants/app_colors.dart';
+
 class DateRangeContainer extends StatelessWidget {
   final DateTime startDate;
   final DateTime endDate;
@@ -17,19 +19,20 @@ class DateRangeContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.lightBlue), // Add border color
+        border: Border.all(color: purple80), // Add border color
         borderRadius: BorderRadius.circular(8), // Rounded borders
       ),
-      padding: EdgeInsets.symmetric(horizontal: 8), // Add padding
+      padding: EdgeInsets.all(0), // Add padding
       child: Row(
         mainAxisSize: MainAxisSize.min, // Minimize the size of the Row
         children: [
+          SizedBox(width: 10),
           Text(
             '${DateFormat('dd/MM/yyyy').format(startDate)} - ${DateFormat('dd/MM/yyyy').format(endDate)}',
-            style: TextStyle(color: Colors.lightBlue),
+            style: TextStyle(color: purple80),
           ),
           IconButton(
-            icon: Icon(Icons.calendar_month, color: Colors.lightBlue),
+            icon: Icon(Icons.calendar_month, color: purple80),
             onPressed: onCalendarPressed, // Calendar button callback
           ),
         ],
