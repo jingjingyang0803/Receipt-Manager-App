2d3da69b69aeca74bb2994e6c8c41085e586898b
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-20 13:56:35 +0200

    Use Consumer to reflect filter list changes

--- lib/screens/summary_page.dart
+++ lib/screens/summary_page.dart
@@ -36,44 +36,36 @@ class SummaryPageState extends State<SummaryPage> {
   ];
 
   final List<int> years =
-      List<int>.generate(20, (index) => 2020 + index); // From 2020 to 2039
+      List<int>.generate(20, (index) => 2000 + index); // From 2020 to 2039
 
   @override
   void initState() {
     super.initState();
+    // Load data when the widget is initialized
+    Future.microtask(() {
+      final receiptProvider =
+          Provider.of<ReceiptProvider>(context, listen: false);
+      final budgetProvider =
+          Provider.of<BudgetProvider>(context, listen: false);
 
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      try {
-        final receiptProvider =
-            Provider.of<ReceiptProvider>(context, listen: false);
-        final budgetProvider =
-            Provider.of<BudgetProvider>(context, listen: false);
+      receiptProvider.fetchAllReceipts();
+      budgetProvider.loadUserBudgets();
 
-        await receiptProvider.fetchAllReceipts();
-        await budgetProvider.loadUserBudgets();
-
-        receiptProvider.groupReceiptsByCategoryOneMonth(_month, _year);
-
-        setState(() {}); // Trigger rebuild after data is ready
-      } catch (e, stackTrace) {
-        logger.e("Error: $e\n$stackTrace");
-      }
+      receiptProvider.groupReceiptsByCategoryOneMonth(_month, _year);
+      receiptProvider.calculateTotalSpending(
+          receiptProvider.groupedReceiptsByCategoryOneMonth!);
     });
   }
 
-  void _loadDataForSelectedDate() async {
-    // Initialize the providers using context
+  void _loadDataForSelectedDate() {
     final receiptProvider =
         Provider.of<ReceiptProvider>(context, listen: false);
 
     logger.i("Loading data for Month: $_month, Year: $_year");
 
-    // Ensure fetchAllReceipts is awaited before grouping
-    await receiptProvider.fetchAllReceipts();
     receiptProvider.groupReceiptsByCategoryOneMonth(_month, _year);
-
-    // Trigger UI update after data is loaded
-    setState(() {});
+    receiptProvider.calculateTotalSpending(
+        receiptProvider.groupedReceiptsByCategoryOneMonth!);
   }
 
   void _showMonthPicker() {
@@ -166,28 +158,6 @@ class SummaryPageState extends State<SummaryPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Initialize the providers using context
-    final receiptProvider =
-        Provider.of<ReceiptProvider>(context, listen: false);
-    final budgetProvider = Provider.of<BudgetProvider>(context, listen: false);
-
-    final budgets = budgetProvider.budgets;
-    receiptProvider.fetchAllReceipts();
-    receiptProvider.groupReceiptsByCategoryOneMonth(_month, _year);
-    final expenses = receiptProvider.groupedReceiptsByCategoryOneMonth;
-    logger.i("Expenses: $expenses");
-
-    receiptProvider.calculateTotalSpending(expenses!);
-    // Calculate total spending
-    final totalSpending = receiptProvider.totalSpending;
-    logger.i("totalSpending: $totalSpending");
-
-    // Access 'currencySymbolToDisplay' from the first entry, assuming all entries share the same currency
-    final currencySymbol = expenses.isNotEmpty
-        ? expenses.values.first['currencySymbolToDisplay']
-        : null; // Handle empty map case receiptProvider.calculateTotalSpending(expenses);
-    logger.i("currencySymbol: $currencySymbol");
-
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: light90,
@@ -197,185 +167,192 @@ class SummaryPageState extends State<SummaryPage> {
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Column(
-        children: [
-          Divider(
-            color: Colors.grey.shade300,
-            thickness: 1,
-            height: 1,
-          ),
-          // Month and Year Picker
-          Container(
-            color: light90,
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      side: BorderSide(color: purple60),
-                    ),
-                  ),
-                  onPressed: _showMonthPicker,
-                  child: Text(
-                    months[_month - 1],
-                    style: TextStyle(fontSize: 16, color: purple60),
-                  ),
-                ),
-                SizedBox(width: 16),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      side: BorderSide(color: purple60),
-                    ),
-                  ),
-                  onPressed: _showYearPicker,
-                  child: Text(
-                    _year.toString(),
-                    style: TextStyle(fontSize: 16, color: purple60),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // Display total spending
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
+      body: Consumer2<ReceiptProvider, BudgetProvider>(
+        builder: (context, receiptProvider, budgetProvider, _) {
+          final budgets = budgetProvider.budgets;
+          final expenses = receiptProvider.groupedReceiptsByCategoryOneMonth;
+          final totalSpending = receiptProvider.totalSpending;
+          final currencySymbol = receiptProvider.allReceipts.isNotEmpty
+              ? receiptProvider.allReceipts.first['currencySymbolToDisplay'] ??
+                  '€'
+              : '€';
+
+          return Column(
+            children: [
+              Divider(
+                color: Colors.grey.shade300,
+                thickness: 1,
+                height: 1,
+              ),
+              // Month and Year Picker
+              Container(
+                color: light90,
+                padding: const EdgeInsets.all(20),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'Total Spending: ',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(color: purple60),
+                        ),
+                      ),
+                      onPressed: _showMonthPicker,
+                      child: Text(
+                        months[_month - 1],
+                        style: TextStyle(fontSize: 16, color: purple60),
+                      ),
                     ),
-                    Text(
-                      '$currencySymbol ${totalSpending.toStringAsFixed(2)}',
-                      style: TextStyle(fontSize: 18, color: Colors.red),
+                    SizedBox(width: 16),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(color: purple60),
+                        ),
+                      ),
+                      onPressed: _showYearPicker,
+                      child: Text(
+                        _year.toString(),
+                        style: TextStyle(fontSize: 16, color: purple60),
+                      ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 4),
-                Text(
-                  'Note: Total includes uncategorized expenses.',
-                  style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+              ),
+              // Display total spending
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Total Spending: ',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '$currencySymbol ${totalSpending.toStringAsFixed(2)}',
+                          style: TextStyle(fontSize: 18, color: Colors.red),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'Note: Total includes uncategorized expenses.',
+                      style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          // Budget and Expense List
-          Expanded(
-            child: ListView.builder(
-              itemCount: budgets.length,
-              itemBuilder: (context, index) {
-                final budget = budgets[index];
-                final categoryId = budget['categoryId'];
-                final categoryName = budget['categoryName'];
-                final categoryIcon = budget['categoryIcon'];
-                final budgetAmount = budget['amount'];
+              ),
+              // Budget and Expense List
+              Expanded(
+                child: ListView.builder(
+                  itemCount: budgets.length,
+                  itemBuilder: (context, index) {
+                    final budget = budgets[index];
+                    final categoryId = budget['categoryId'];
+                    final categoryName = budget['categoryName'];
+                    final categoryIcon = budget['categoryIcon'];
+                    final budgetAmount = budget['amount'];
 
-                logger.i("expenses: $expenses");
-                final spent = expenses[categoryId]?['total'] ?? 0.0;
-                final currencySymbol =
-                    expenses[categoryId]?['currencySymbolToDisplay'] ?? ' ';
+                    final spent = expenses![categoryId]?['total'] ?? 0.0;
+                    final categoryCurrencySymbol =
+                        expenses[categoryId]?['currencySymbolToDisplay'] ?? '€';
 
-                logger.i("Budget: $budget");
-                logger.i(
-                    "CategoryId: $categoryId, Spent: ${expenses[categoryId]?['totalToDisplay']}");
+                    double ratio = budgetAmount == 0
+                        ? (spent > 0 ? 1.0 : 0.0)
+                        : spent / budgetAmount;
+                    String ratioText = budgetAmount == 0
+                        ? (spent > 0 ? '∞%' : '0.0%')
+                        : '${(ratio * 100).toStringAsFixed(1)}%';
 
-                double ratio = budgetAmount == 0
-                    ? (spent > 0 ? 1.0 : 0.0)
-                    : spent / budgetAmount;
-                String ratioText = budgetAmount == 0
-                    ? (spent > 0 ? '∞%' : '0.0%')
-                    : '${(ratio * 100).toStringAsFixed(1)}%';
-
-                return Card(
-                  color: Colors.white,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                  elevation: 2,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 16),
-                    leading: Container(
-                      width: 8,
-                      height: double.infinity,
-                      color: getColor(ratio),
-                    ),
-                    title: Row(
-                      children: [
-                        Text(categoryIcon, style: TextStyle(fontSize: 26)),
-                        SizedBox(width: 8),
-                        Text(categoryName,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w700)),
-                      ],
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    return Card(
+                      color: Colors.white,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 8),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 16),
+                        leading: Container(
+                          width: 8,
+                          height: double.infinity,
+                          color: getColor(ratio),
+                        ),
+                        title: Row(
                           children: [
-                            Text('Budget:',
+                            Text(categoryIcon, style: TextStyle(fontSize: 26)),
+                            SizedBox(width: 8),
+                            Text(categoryName,
                                 style: TextStyle(
-                                    fontSize: 14, color: Colors.grey[700])),
-                            Text(
-                              '$currencySymbol ${budgetAmount.toStringAsFixed(2)}',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.grey[800]),
+                                    fontSize: 16, fontWeight: FontWeight.w700)),
+                          ],
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Budget:',
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey[700])),
+                                Text(
+                                  '$categoryCurrencySymbol ${budgetAmount.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.grey[800]),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 4),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Spent:',
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey[700])),
+                                Text(
+                                  '$categoryCurrencySymbol ${spent.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: getColor(ratio),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 4),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Percentage:',
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey[700])),
+                                Text(
+                                  ratioText,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: getColor(ratio),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        SizedBox(height: 4),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Spent:',
-                                style: TextStyle(
-                                    fontSize: 14, color: Colors.grey[700])),
-                            Text(
-                              '$currencySymbol ${spent.toStringAsFixed(2)}',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: getColor(ratio),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 4),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Percentage:',
-                                style: TextStyle(
-                                    fontSize: 14, color: Colors.grey[700])),
-                            Text(
-                              ratioText,
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: getColor(ratio),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
