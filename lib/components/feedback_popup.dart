e2b9f5e5d40f2c84fbbc8a258f0b462619b997d8
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-25 15:40:50 +0200

    Create FeedbackDialog and apply

---
+++ lib/components/feedback_popup.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
+import 'custom_button.dart';
+import 'custom_divider.dart';
+
+class FeedbackDialog extends StatelessWidget {
+  final VoidCallback onCancel;
+  final VoidCallback onSubmit;
+  final TextEditingController feedbackController;
+
+  const FeedbackDialog({
+    required this.onCancel,
+    required this.onSubmit,
+    required this.feedbackController,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const CustomDivider(),
+          SizedBox(height: 8),
+          Text(
+            'Submit Feedback',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'We value your feedback! Please share your thoughts below.',
+            style: TextStyle(
+              fontSize: 16,
+              color: purple200,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 12),
+          TextField(
+            controller: feedbackController,
+            maxLines: 3,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              hintText: 'Enter your feedback here...',
+              contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomButton(
+                    text: "Cancel",
+                    backgroundColor: purple20,
+                    textColor: purple100,
+                    onPressed: onCancel,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomButton(
+                    text: "Submit",
+                    backgroundColor: purple100,
+                    textColor: light80,
+                    onPressed: onSubmit,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
